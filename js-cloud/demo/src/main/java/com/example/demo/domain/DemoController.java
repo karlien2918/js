fab94f239160b434ac1da293a861d6dcbb42feac
jinsungKim <jskim@kr.doubledown.com>
2025-04-03 16:53:42 +0900

    delete

---
+++ js-cloud/demo/src/main/java/com/example/demo/domain/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+
+	@GetMapping
+	public ResponseEntity<String> index(){
+		return ResponseEntity.ok("Hello World");
+	}
+}
